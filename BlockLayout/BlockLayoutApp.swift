158a6f3ed4a6d447d2c507a3617f3b82afa36257
Stellashin <shinseohyun0530@gmail.com>
2026-02-05 19:59:09 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by zimmer_h on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
